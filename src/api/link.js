ce0162ef16e805576a7c2c02d9486dd8ecc80286
66ds <1532498760@qq.com>
2020-12-17 22:37:33 +0800

    '111'

--- src/api/link.js
+++ src/api/link.js
@@ -7,8 +7,56 @@ import request from '../utils/request';
  */
 export const selectFriendLinkListApi = (query) => {
     return request({
-        url:'/api/v1/pub/friendlylink/lists',
+        url:'/api/v1/pri/friendlylink/lists',
         method:'post',
         data:query
     })
 }
+
+/**
+ * 批量删除友链
+ * @param stayIds
+ */
+export const linkDeleteBatchApi = (linkIds)=>{
+    return request({
+        url:"/api/v1/pri/friendlylink/batch?linkIds="+linkIds,
+        method:"get"
+    })
+}
+
+/**
+ * 单个友链删除
+ * @param linkId
+ */
+export const linkDeleteApi = (linkId)=>{
+    return request({
+        url:"/api/v1/pri/friendlylink/delete/"+linkId,
+        method:"post"
+    })
+}
+
+/**
+ * 单个友链查询
+ * @param linkId
+ * @returns {AxiosPromise}
+ */
+export const selectLinkInfoApi = (linkId)=>{
+    return request({
+        url:"/api/v1/pri/friendlylink/info/"+linkId,
+        method:"post"
+    })
+}
+
+/**
+ * 修改友链信息
+ * @param linkForm
+ * @returns {AxiosPromise}
+ */
+export const updateLinkInfoApi = (linkForm) => {
+    return request({
+        url:"/api/v1/pri/friendlylink/update",
+        method:"post",
+        data:linkForm
+    })
+}
+
--- src/components/page/Link.vue
+++ src/components/page/Link.vue
@@ -30,10 +30,14 @@
             >
                 <el-table-column type="selection" width="55" align="center"></el-table-column>
                 <el-table-column prop="linkId" label="友链标识" align="center"></el-table-column>
-                <el-table-column prop="linkName" label="友链名称" align="center" :show-overflow-tooltip="true"></el-table-column>
-                <el-table-column prop="linkUrl" label="友链地址" align="center" :show-overflow-tooltip="true"></el-table-column>
-                <el-table-column prop="linkEmail" label="友链邮箱" align="center" :show-overflow-tooltip="true"></el-table-column>
-                <el-table-column prop="linkDesc" label="友链信息" align="center" :show-overflow-tooltip="true"></el-table-column>
+                <el-table-column prop="linkName" label="友链名称" align="center"
+                                 :show-overflow-tooltip="true"></el-table-column>
+                <el-table-column prop="linkUrl" label="友链地址" align="center"
+                                 :show-overflow-tooltip="true"></el-table-column>
+                <el-table-column prop="linkEmail" label="友链邮箱" align="center"
+                                 :show-overflow-tooltip="true"></el-table-column>
+                <el-table-column prop="linkDesc" label="友链信息" align="center"
+                                 :show-overflow-tooltip="true"></el-table-column>
                 <el-table-column prop="linkCreateTime" label="友链创建时间" align="center"></el-table-column>
                 <el-table-column prop="linkAllow" label="是否审核" align="center">
                     <template slot-scope="scope">
@@ -47,7 +51,7 @@
                         <el-button
                                 type="text"
                                 icon="el-icon-edit"
-                                @click="handleDelete(scope.$index, scope.row)"
+                                @click="handleEdit(scope.$index, scope.row)"
                         >编辑
                         </el-button>
                         <el-button
@@ -71,65 +75,122 @@
                 ></el-pagination>
             </div>
         </div>
-        <!-- 分类的填写和编辑-->
-        <el-dialog title="回复" :visible.sync="addStay" width="60%" @close="closeDialog">
-            <el-form :model="stayForm" :rules="rules" ref="stayForm" label-width="80px" label-position="right"
+        <!-- 友链的编辑-->
+        <el-dialog title="文章" :visible.sync="addLink" width="60%" @close="closeDialog">
+            <el-form :model="linkForm" :rules="rules" ref="linkForm" label-width="80px" label-position="right"
                      class="demo-ruleForm">
-                <el-form-item label="留言内容" prop="messageContent">
-                    <el-input type="textarea" v-model="stayForm.messageContent" maxlength="100"  show-word-limit :autosize="{ minRows: 4, maxRows: 5}"></el-input>
+                <el-form-item label="友链名称" prop="linkName">
+                    <el-input v-model="linkForm.linkName" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="友链地址" prop="linkUrl">
+                    <el-input v-model="linkForm.linkUrl" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="友链邮箱" prop="linkEmail">
+                    <el-input v-model="linkForm.linkEmail" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="友链信息" prop="linkDesc">
+                    <el-input v-model="linkForm.linkDesc" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="是否审核" prop="linkAllow">
+                    <el-switch v-model="linkForm.linkAllow" :inactive-value="0" :active-value="1"></el-switch>
                 </el-form-item>
             </el-form>
             <div slot="footer" class="dialog-footer">
-                <el-button @click="addStay = false">取 消</el-button>
-                <el-button type="primary" @click="submitForm('stayForm')">{{flag==true?'添加':'编辑'}}</el-button>
+                <el-button @click="addLink = false">取 消</el-button>
+                <el-button type="primary" @click="submitForm('linkForm')">编辑</el-button>
             </div>
         </el-dialog>
     </div>
 </template>
 
 <script>
-    import {selectFriendLinkListApi,addStayMessageApi,selectStayInfoApi,updateStayInfoApi,stayDeleteBatchApi,stayDeleteApi} from '../../api/link.js'
+    import {
+        selectFriendLinkListApi,
+        selectLinkInfoApi,
+        updateLinkInfoApi,
+        linkDeleteBatchApi,
+        linkDeleteApi
+    } from '../../api/link.js';
+
+    let checkEmail = (rule, value, callback) => {
+        const mailReg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/
+        if (!value) {
+            return callback(new Error('请输入邮箱地址'))
+        }
+        setTimeout(() => {
+            if (mailReg.test(value)) {
+                callback()
+            } else {
+                callback(new Error('请输入正确的邮箱格式'))
+            }
+        }, 100)
+    };
+    let checkUrl = (rule, value, callback) => {
+        const reg = new RegExp(
+            /^http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/
+        )
+        if (value === '') {
+            callback(new Error('请输入站点地址'))
+        } else if (!reg.test(value)) {
+            callback(new Error('请输入正确的站点地址'))
+        } else {
+            callback()
+        }
+    }
     export default {
         name: 'basetable',
         data() {
             return {
                 query: {
-                    content:'',
+                    content: '',
                     page: 1,
                     limit: 10
                 },
-                addStay: false,
-                loading:true,
+                addLink: false,
+                loading: true,
                 tableData: [],
                 delList: [],
                 pageTotal: 0,
-                flag: true,
-                stayForm: {
-                    stayId: '',
-                    messageContent: '',
-                    parentStayId:''
+                linkForm: {
+                    linkId: '',
+                    linkName: '',
+                    linkUrl: '',
+                    linkEmail:'',
+                    linkDesc:'',
+                    linkAllow:0
                 },
                 rules: {
-                    messageContent: [
-                        { required: true, message: '请输入留言内容', trigger: 'blur' }
-                    ]
-                },
+                    linkName: [
+                        { required: true, message: '请输入站点名称', trigger: 'blur' },
+                    ],
+                    linkUrl: [
+                        { required: true,validator: checkUrl, trigger: 'change' }
+                    ],
+                    linkEmail: [
+                        { required:true, validator: checkEmail,trigger: 'change' }
+                    ],
+                    linkDesc: [
+                        { required: true,message: '请输入站点描述', trigger: 'blur' }
+                    ],
+                    linkAllow: [
+                        { required: true,trigger: 'blur' },
+                    ],
+                }
             };
         },
-        components: {
-        },
+        components: {},
         created() {
             this.getData(this.query);
         },
         methods: {
             // 获取文章列表数据
             getData(query) {
-                this.loading = true
+                this.loading = true;
                 selectFriendLinkListApi(query).then(res => {
                     this.tableData = res.data.list;
                     this.pageTotal = res.data.totalCount || 0;
                     this.loading = false;
-                }).catch(e=>{
+                }).catch(e => {
                     this.$message.error(e);
                 });
             },
@@ -140,7 +201,7 @@
             },
             // 多选操作
             handleSelectionChange(val) {
-                val = val.map(item => item.stayId)
+                val = val.map(item => item.linkId);
                 this.delList = val;
             },
             // 分页导航
@@ -148,100 +209,78 @@
                 this.$set(this.query, 'page', val);
                 this.getData(this.query);
             },
+            //编辑操作
+            handleEdit(index, row) {
+                //打开弹窗
+                this.addLink = true;
+                //获取单个友链信息
+                this.selectLinkInfo(row.linkId);
+            },
             //关闭弹窗重置表单
             closeDialog() {
-                this.$refs.stayForm.resetFields();
+                this.$refs.linkForm.resetFields();
             },
-            //批量删除留言
+            //批量删除友链
             delAllSelection() {
-                if(this.delList.length == 0){
-                    return  this.$message.warning("请先选择");
+                if (this.delList.length == 0) {
+                    return this.$message.warning('请先选择');
                 }
                 // 二次确认删除
                 this.$confirm(`确定要删除${this.delList}吗？`, '提示', {
                     type: 'warning'
                 }).then(() => {
-                    return stayDeleteBatchApi(this.delList);
+                    return linkDeleteBatchApi(this.delList);
                 }).then(res => {
-                    if (res.code == 0) {
-                        this.$message.success(res.msg);
-                        this.handleSearch()
-                    }
+                    this.$message.success(res.msg);
+                    this.handleSearch();
                 }).catch((e) => {
                     this.$message.error(e);
                 });
             },
-            // 删除单个留言
+            // 删除单个友链
             handleDelete(index, row) {
                 // 二次确认删除
                 this.$confirm('确定要删除吗？', '提示', {
                     type: 'warning'
                 }).then(() => {
-                    return stayDeleteApi(row.stayId);
+                    return linkDeleteApi(row.linkId);
                 }).then(res => {
                     this.$message.success(res.msg);
-                    this.handleSearch()
+                    this.handleSearch();
                 }).catch((e) => {
                     this.$message.error(e);
                 });
             },
-            //发表回复
-            publishReply(index,row){
-                this.flag = true;
-                this.addStay = true;
-                this.stayForm.parentStayId = row.stayId;
-            },
-            //查看留言
-            checkReply(index,row){
-                this.flag = false
-                this.addStay = true
-                this.selectStayInfo(row.stayId)
-            },
-            //添加和编辑
+            //编辑
             submitForm(formName) {
                 this.$refs[formName].validate((valid) => {
                     if (valid) {
-                        if (this.flag) {
-                            this.addStayMessage(this.stayForm.messageContent,this.stayForm.parentStayId)
-                        } else {
-                            this.updateStayInfo(this.stayForm)
-                        }
+                        this.updateLinkInfo(this.linkForm);
                     } else {
                         return false;
                     }
                 });
             },
-            //公共模块 添加留言并重新刷新留言
-            async addStayMessage(content,parentStayId){
-                try{
-                    const res = await addStayMessageApi(content,parentStayId);
-                    this.$message.success(res.msg);
-                    //关闭弹窗
-                    this.addStay = false
-                    //重新刷新表格
-                    this.handleSearch()
-                }catch (e) {
-                    this.$message.error(e)
-                }
-            },
-            //查看parentstayId是stayId的留言信息
-            async selectStayInfo(stayId){
-                try{
-                    const res = await selectStayInfoApi(stayId);
-                    this.stayForm = res.data;
-                }catch (e) {
-                    this.$message.error(e)
-                }
-            },
             //修改留言内容
-            async updateStayInfo(stayForm){
-                try{
-                    const res = await updateStayInfoApi(stayForm);
+            async updateLinkInfo(linkForm) {
+                try {
+                    const res = await updateLinkInfoApi(linkForm);
                     this.$message.success(res.msg);
                     //关闭弹窗
-                    this.addStay = false
-                }catch (e) {
-                    this.$message.error(e)
+                    this.addLink = false;
+                    //刷新内容
+                    this.handleSearch();
+                } catch (e) {
+                    this.$message.error(e);
+                }
+            },
+            //查询单个友链信息
+            async selectLinkInfo(linkId) {
+                try {
+                    const res = await selectLinkInfoApi(linkId);
+                    this.linkForm = res.data;
+                } catch (e) {
+                    this.$message.error(e);
                 }
             }
 
@@ -250,11 +289,12 @@
 </script>
 
 <style>
-    @media only screen and (min-width: 321px) and (max-width: 768px){
-        .link .handle-box{
+    @media only screen and (min-width: 321px) and (max-width: 768px) {
+        .link .handle-box {
             display: none;
         }
     }
+
     .link .handle-box {
         margin-bottom: 20px;
     }
